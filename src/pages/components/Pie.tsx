8d53dc7e5927ebf1e867266dd06e90baeb406a82
lipu <lipu.lp@aliyun.com>
2021-06-15 01:02:41 +0800

    Revert "测试撤销提交"
    This reverts commit be7aa84347232add81c8db74382276b401fffe7b.

--- src/pages/components/Pie.tsx
+++ src/pages/components/Pie.tsx
@@ -12,7 +12,7 @@ class Pie extends React.Component {
   }
    fun() {
     const data = [
-      { item: '事例一', count: 40, percent: 0.4 },
+      { item: '事例一', count: 40, percent: 0.4 },  
       { item: '事例二', count: 21, percent: 0.21 },
       { item: '事例三', count: 17, percent: 0.17 },
       { item: '事例四', count: 13, percent: 0.13 },
